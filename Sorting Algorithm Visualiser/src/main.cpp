fdd90d2d995181dd1d2d30ff43021c7bdb6934ab
niranjan-gurung <gurung5130@gmail.com>
2022-06-21 18:43:01 +0100

    graph representation of random numbers displayed

--- Sorting Algorithm Visualiser/src/main.cpp
+++ Sorting Algorithm Visualiser/src/main.cpp
@@ -1,17 +1,21 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
-#include <array>
+#include <vector>
+#include <algorithm>
+#include <random>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Sorting Algorithm Visualiser!");
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Sorting Algorithm Visualiser!");
     window.setVerticalSyncEnabled(true);
-    
+   
     int counter = 0;
 
     sf::Font font;
     if (!font.loadFromFile("./font/arial.ttf"))
+    {
         std::cout << "Couldn't load font.." << std::endl;
+    }
 
     sf::Text t;
     t.setFont(font);
@@ -19,26 +23,69 @@ int main()
     t.setFillColor(sf::Color::Red);
     t.setString("Counter = " + std::to_string(counter));
     t.setOrigin(t.getGlobalBounds().width/2, t.getGlobalBounds().height/2);
-    t.setPosition(800/2, 600/2);
+    t.setPosition(800/2, 200);
 
-    std::array<int, 10> a = { 5, 7, 10, 2, 37, 21, 1, 58, 44, 2 };
+    const size_t MAX_SIZE = 30;
+
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<uint32_t> generate(10, 200);
+
+    // init empty lists:
+    std::vector<uint32_t> randomNumberList(MAX_SIZE);
+    std::vector<sf::RectangleShape> graph(MAX_SIZE);
+    
+    int c = 0;                      // index tracker of graph list
+    float barGraphSpacing = 10;     // tracks spacing between each individual graph's bars.
+
+    for_each(randomNumberList.begin(), randomNumberList.end(), 
+        [&](uint32_t& randomNumberList) 
+    { 
+        // generate random numbers for the array list:
+        randomNumberList = generate(rng);
+
+        // generate rectagle height based on list of numbers:
+        graph.at(c) = sf::RectangleShape(sf::Vector2f(20.f, randomNumberList));
+        graph.at(c).setFillColor(sf::Color::Green);
+        graph.at(c).setPosition(
+            barGraphSpacing, 
+            window.getSize().y-graph.at(c).getGlobalBounds().height
+        );
+
+        c++;
+        barGraphSpacing += 30;
+    });
+
+    // size alias:
+    size_t listSize = randomNumberList.size();
 
     // bubble sort algorithm
-    for (size_t i = 0; i < 10; i++) 
+    for (size_t i = 0; i < listSize; i++) 
     {
-        for (size_t j = 0; j < 10 - 1; j++)
+        for (size_t j = 0; j < listSize-1; j++)
         {
-            if (a[j] > a[j + 1])
+            if (randomNumberList[j] > randomNumberList[j+1])
             {
-                int temp = a[j];
-                a[j] = a[j+1];
-                a[j+1] = temp;
+                // swap from the numbers list:
+                std::swap(randomNumberList[j], randomNumberList[j+1]);
+
+                // swap from the graph list:
+                /*sf::RectangleShape r = graph[j];
+                graph[j] = graph[j+1];
+                graph[j+1] = r;*/
+
+                graph[j].setFillColor(sf::Color::Red);
+                graph[j+1].setFillColor(sf::Color::Blue);
+                /*sf::Vector2f temp(graph[j].getPosition().x, graph[j].getPosition().y);
+                graph[j].setPosition(graph[j+1].getPosition().x, graph[j+1].getPosition().y);
+                graph[j+1].setPosition(temp.x, temp.y);*/
+                //std::swap(graph[j], graph[j+1]);
             }
         }
     }
 
-    for (const auto& v : a)
-        std::cout << v << std::endl;
+    for (const auto& value : randomNumberList)
+        std::cout << value << std::endl;
 
     while (window.isOpen())
     {
@@ -49,11 +96,20 @@ int main()
                 window.close();
         }
 
-        sf::sleep(sf::milliseconds(100));
-        t.setString("Counter = " + std::to_string(counter++));
+        //sf::sleep(sf::milliseconds(100));
+        //t.setString("Counter = " + std::to_string(counter++));
 
         window.clear(sf::Color::White);
         window.draw(t);
+        
+        for (const auto& value : graph)
+        {
+            window.draw(value);
+        }
+
+        //window.draw(a);
+        //window.draw(b);
+
         window.display();
     }
 }
